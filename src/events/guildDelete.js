8c5fef3678edd16da3d7e142509507be0f2b4ec4
Fadix9 <fadi2010fadi12@gmail.com>
2026-06-13 02:32:50 +0200

    Notify dashboard webhook on guild delete

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -5,6 +5,8 @@ import TicketGuildConfig from '../models/TicketGuildConfig.js';
 import VoiceChannel from '../models/VoiceChannel.js';
 import GuildSetup from '../models/GuildSetup.js';
 
+const DASHBOARD_URL = process.env.DASHBOARD_URL || 'http://localhost:10001';
+
 export const name = Events.GuildDelete;
 export const once = false;
 
@@ -18,6 +20,12 @@ export async function execute(guild) {
     TicketGuildConfig.deleteOne({ guildId }),
     VoiceChannel.deleteMany({ guildId }),
     GuildSetup.deleteOne({ guildId }),
+    // Also notify dashboard to clean its data
+    fetch(`${DASHBOARD_URL}/api/webhooks/guild-delete`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ guildId, secret: process.env.BOT_TOKEN }),
+    }).catch(() => {}),
   ]);
 
   console.log(`[GuildDelete] Cleaned data for ${guild.name} (${guildId})`);
