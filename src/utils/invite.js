da74029acd69cddd9c4d5db66f71a94553a1864f
Fadix9 <fadi2010fadi12@gmail.com>
2026-06-14 02:22:39 +0200

    تثبيت رابط سرفر الدعم https://discord.gg/ENQut4HHKb في رسائل التبنيد

--- src/utils/invite.js
+++ src/utils/invite.js
@@ -1,20 +1,3 @@
-import { PermissionFlagsBits } from 'discord.js';
-
-export async function getGuildInvite(guild) {
-  try {
-    const vanity = await guild.fetchVanityData();
-    if (vanity?.code) return `https://discord.gg/${vanity.code}`;
-  } catch {}
-
-  try {
-    const target = guild.systemChannel || guild.channels.cache.find(c =>
-      c.type === 0 && c.permissionsFor(guild.members.me).has(PermissionFlagsBits.CreateInstantInvite)
-    );
-    if (target) {
-      const invite = await target.createInvite({ maxAge: 0, maxUses: 0 });
-      return invite.url;
-    }
-  } catch {}
-
-  return null;
+export async function getGuildInvite(_guild) {
+  return 'https://discord.gg/ENQut4HHKb';
 }
